d7592e322ee3dfe8f9da26fa3d3d221c76f79ac0
Wilson Fernando Pena <wilsonf.pena@gmail.com>
2023-08-14 15:07:34 -0300

    feat: windows style exit button and sorting pipe

--- src/app/previous-games/previous-games.component.spec.ts
+++ src/app/previous-games/previous-games.component.spec.ts
@@ -23,33 +23,46 @@ describe('PreviousGamesComponent', () => {
   describe('PreviousGamesPipe', () => {
     it('should return empty array for empty array input', () => {
       const pipe = new PreviousGamesPipe();
-      const resp = pipe.transform([], 'aaaa');
+      const resp = pipe.transform([]);
       expect(resp).toEqual([]);
     });
 
     it('should sort by difficulty ascending - case 1', () => {
       const pipe = new PreviousGamesPipe();
-      const resp = pipe.transform([{id:'1', difficulty: 'easy'}] as GameStats[], 'difficulty');
+      const resp = pipe.transform([{id:'1', difficulty: 'easy'}] as GameStats[]);
       expect(resp).toEqual([{id:'1', difficulty: 'easy'}]);
     });
 
     it('should sort by difficulty ascending - case 2', () => {
       const pipe = new PreviousGamesPipe();
-      const resp = pipe.transform([{id:'1', difficulty: 'easy'},{id:'2',difficulty: 'hard'}] as GameStats[], 'difficulty');
+      const resp = pipe.transform([{id:'1', difficulty: 'easy'},{id:'2',difficulty: 'hard'}] as GameStats[]);
       expect(resp).toEqual([{id:'2',difficulty: 'hard'}, {id:'1', difficulty: 'easy'}]);
     });
 
     it('should sort by difficulty ascending - case 3', () => {
       const pipe = new PreviousGamesPipe();
-      const resp = pipe.transform([{id:'1', difficulty: 'easy'},{id:'2',difficulty: 'hard'},{id:'3',difficulty: 'hard'}] as GameStats[], 'difficulty');
+      const resp = pipe.transform([{id:'1', difficulty: 'easy'},{id:'2',difficulty: 'hard'},{id:'3',difficulty: 'hard'}] as GameStats[]);
       expect(resp).toEqual([{id:'2',difficulty: 'hard'}, {id:'3',difficulty: 'hard'}, {id:'1', difficulty: 'easy'}]);
     });
 
-    // TODO: Sort by time spent
-    // it('should sort by difficulty and total time ascending - case 1', () => {
-    //   const pipe = new PreviousGamesPipe();
-    //   const resp = pipe.transform([{id:'1', difficulty: 'easy', totalTime: 1},{id:'2',difficulty: 'hard'},{id:'3',difficulty: 'hard'}] as GameStats[], 'difficulty');
-    //   expect(resp).toEqual([{id:'2',difficulty: 'hard'}, {id:'3',difficulty: 'hard'}, {id:'1', difficulty: 'easy'}]);
-    // });
+    it('should sort by difficulty and total time ascending - case 1', () => {
+      const pipe = new PreviousGamesPipe();
+      const resp = pipe.transform([
+        {id:'1', difficulty: 'easy', totalTime: 1},{id:'2',difficulty: 'hard',totalTime: 12},{id:'3',difficulty: 'hard',totalTime:2},{id:'4',difficulty:'medium',totalTime:0},{id:'5', difficulty: 'easy', totalTime: 12}
+      ] as GameStats[]);
+      expect(resp).toEqual([
+        {id:'3',difficulty: 'hard',totalTime:2},{id:'2',difficulty: 'hard',totalTime: 12},{id:'4',difficulty:'medium',totalTime:0},{id:'1', difficulty: 'easy', totalTime: 1},{id:'5', difficulty: 'easy', totalTime: 12}
+      ]);
+    });
+
+    it('should sort by difficulty and total time ascending - case 2', () => {
+      const pipe = new PreviousGamesPipe();
+      const resp = pipe.transform([
+        {id:'1', difficulty: 'hard', totalTime: 1},{id:'2',difficulty: 'hard',totalTime: 12},{id:'3',difficulty: 'hard',totalTime:2},{id:'4',difficulty:'medium',totalTime:0},{id:'5', difficulty: 'easy', totalTime: 12}
+      ] as GameStats[]);
+      expect(resp).toEqual([
+        {id:'1', difficulty: 'hard', totalTime: 1},{id:'3',difficulty: 'hard',totalTime:2},{id:'2',difficulty: 'hard',totalTime: 12},{id:'4',difficulty:'medium',totalTime:0},{id:'5', difficulty: 'easy', totalTime: 12}
+      ]);
+    });
   });
 });
